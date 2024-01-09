9024e8f6c931aa2e3840c03e9a8be5350f6bf49d
AbdullahKus <62968246+abdks@users.noreply.github.com>
2024-01-09 23:53:32 +0300

    9ocak

--- MyMangoDbAjaxProject/ViewComponents/UILayout/_MainPageComponentPartial.cs
+++ MyMangoDbAjaxProject/ViewComponents/UILayout/_MainPageComponentPartial.cs
@@ -7,19 +7,19 @@ namespace MyMangoDbAjaxProject.ViewComponents.UILayout
 {
     public class _MainPageComponentPartial : ViewComponent
     {
-        private readonly IMongoCollection<About> _aboutcollection;
+        private readonly IMongoCollection<MainHome> _MainHomecollection;
 
         public _MainPageComponentPartial(IDatabaseSettings _databaseSettings)
         {
             var client = new MongoClient(_databaseSettings.ConnectionString);
             var database = client.GetDatabase(_databaseSettings.DatabaseName);
-            _aboutcollection = database.GetCollection<About>(_databaseSettings.AboutCollectionName);
+            _MainHomecollection = database.GetCollection<MainHome>(_databaseSettings.MainHomeCollectionName);
         }
 
         // _MainPageComponentPartial.cs dosyanızdaki InvokeAsync metodu
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            var values = await _aboutcollection.Find(x => true).ToListAsync();
+            var values = await _MainHomecollection.Find(x => true).ToListAsync();
             return View(values);
         }
 
